ad7f7e8707ff10c2952f3c7114feee7960c27bdf
Charles Yun <46541995+c-yun@users.noreply.github.com>
2019-02-19 15:10:03 -0800

    Update clues.sql

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -60,6 +60,7 @@ select * from city where name like 'Serr%' AND not name = 'Serravalle';
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
 
+
 -- Our play date of late has been unusually fun –
 -- As an agent, I'll say, you've been a joy to outrun.
 -- And while the food here is great, and the people – so nice!
@@ -69,7 +70,8 @@ select * from city where name like 'Serr%' AND not name = 'Serravalle';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+Answer: 
+select name from city where population = 91084;
 
 
-
--- She's in ____________________________!
+-- She's in ___Santa Monica_________________________!
